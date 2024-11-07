bdc619b6f20d5a60292176b124a6526457f5b232
Stalyn0101 <yixon11@gmail.com>
2024-11-07 13:34:33 -0500

    hola

---
+++ src/andreachaquinga2026/AndreaChaquinga2026.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package andreachaquinga2026;
+
+/**
+ *
+ * @author SO-LAB1-PC7
+ */
+public class AndreaChaquinga2026 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
